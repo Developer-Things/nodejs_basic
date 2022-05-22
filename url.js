a396c48933489eace656c43d316f8559e5438087
Developer-Things <101121374+Developer-Things@users.noreply.github.com>
2022-05-22 08:55:31 +0530

    url_concepts

---
+++ url.js
@@ -0,0 +1,6 @@
+// var start=require('url')
+// var datas='https://docs.mongodb.com/manual?tutorial/insert-documents/'
+// var result=start.parse(datas,true)
+// console.log(result.host)
+// console.log(result.pathname)
+// console.log(result.search)
